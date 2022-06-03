42c39ff1926ed79ab1e136b4aeba34bb1f819616
Cestnut <hunterkudo@gmail.com>
2022-06-03 21:49:44 +0200

    story reactions added (working on thoughts)

--- chapter.php
+++ chapter.php
@@ -28,6 +28,42 @@
                     #exit();
                 }
             }
+
+            #get all existing reactions
+            $sql = "SELECT reaction, reaction_code FROM reactions";
+            if($result = $conn->query($sql))
+                while($tmp = $result->fetch_array(MYSQLI_ASSOC))
+                        $reactions[] = $tmp;
+
+            #get all thoughts of this chapter and builds thoughts tree
+            $sql = "SELECT thought_ID, thought_padre_ID FROM thoughts WHERE chapter_ID='$chapter_ID'";
+            if($result = $conn->query($sql)){
+                $thoughts = array(); 
+                while($tmp = $result->fetch_array(MYSQLI_ASSOC)){
+                    #print_r($tmp);
+                        $tmp['children']=array();
+                        $thoughts[$tmp['thought_ID']] = $tmp;
+                    }
+                    
+                    print_r($thoughts);
+                foreach ($thoughts as $k => &$v) {
+                        if (isset($v['thought_padre_ID'])) {
+                          $thoughts[$v['thought_padre_ID']]['children'][] = &$v;
+                        }
+                      }
+                     #print_r($thoughts);
+                      #print_r($thoughts);
+                foreach ($thoughts as $k => $v) {
+                        #echo $k;
+                    if (isset($v['thought_padre_ID'])){
+                        unset($thoughts[$k]);
+                        #print_r($thoughts);
+
+                        }
+                      }    
+                      #print_r($thoughts);               
+                }
+
 ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -47,6 +83,25 @@ $(document).ready(function(){
     }
     )
   });
+
+  $(".react_chapter").on("click", function(){
+
+    
+    var reaction = $(this).attr('reaction');
+    $(this).toggleClass("btn-primary");
+
+    $.post("hidden/react_chapter.php",
+    {
+        chapter_id:<?php echo $chapter_ID;?>,
+        reaction:reaction
+    },
+    function(data,status){
+        document.getElementById("chapter_reactions_number"+reaction).innerHTML = 
+        parseInt(document.getElementById("chapter_reactions_number"+reaction).innerHTML)+parseInt(data);  
+    }
+    )
+    });
+
 });
 
 </script>
@@ -56,19 +111,35 @@ $(document).ready(function(){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet">
-    <?php echo '<title>'.$row['story_title'].'- Lalèo</title>'; ?>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">    
+    <?php echo '<title>'.$row['story_title'].' - '.$row['chapter_title'].'</title>'; ?>
 </head>
 <body>
         <div class="box">
             <div class="post-head">
-                <a class="post-author"><?php echo $row['author'];?></a>
+                <a class="post-author" href="profile.php?id=<?php echo $row['author_ID'];?>"><?php echo $row['author'];?></a>
             </div>
             <p class="post-title"><?php echo $row['chapter_title'];?></p>
             <p class="post-content-preview"><?php echo $row['content'];?></p>
             <div class="reactions">
-            <p class="react">5 ❤️</p>
-            <p class="react">3 🎉</p>
-            <p class="react">2 🔥</p>           
+            <?php 
+                foreach($reactions as $reaction){
+                $reaction_code = $reaction['reaction_code'];
+                $reaction = $reaction['reaction'];
+                $sql1 = "SELECT * FROM chapters_accounts_reactions WHERE chapter_ID='$chapter_ID' AND reaction_code='$reaction_code'";
+                $sql2 = "SELECT * FROM chapters_accounts_reactions WHERE chapter_ID='$chapter_ID' AND reaction_code='$reaction_code' AND account_ID='$_SESSION[id]'";
+                if($conn->query($sql2)->num_rows)
+                    echo '<div class="reactions react react_chapter btn-primary" reaction="'.$reaction_code.'">
+                                <div id="chapter_reactions_number'.$reaction_code.'">'.$conn->query($sql1)->num_rows.'</div>
+                            '.$reaction.'
+                            </div>';
+                else
+                    echo '<div class="reactions react react_chapter" reaction="'.$reaction_code.'">
+                                <div id="chapter_reactions_number'.$reaction_code.'">'.$conn->query($sql1)->num_rows.'</div>
+                            '.$reaction.'
+                            </div>';
+                }
+            ?>
             </div>
         </div>
         <div chap_id=<?php echo $_GET['id'];?>><button class="vote" id="up">UP</button></div>
