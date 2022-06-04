7cdb7d9028c695ae7cfdb6dbab8e3fa93f7627df
Cestnut <hunterkudo@gmail.com>
2022-06-04 22:50:07 +0200

    fixes

--- hidden/react_chapter.php
+++ hidden/react_chapter.php
@@ -3,16 +3,16 @@
     session_start();
 
     $chapter_id = $conn->real_escape_string($_POST['chapter_id']);
-    $reaction_code = $conn->real_escape_string($_POST['reaction']);
+    $reaction = $conn->real_escape_string($_POST['reaction']);
     $account_ID = $_SESSION['id'];
     
     try{
-        $sql = "INSERT INTO chapters_accounts_reactions (reaction_code, chapter_ID, account_ID) VALUES ('$reaction_code','$chapter_id','$account_ID')";
+        $sql = "INSERT INTO chapters_accounts_reactions (reaction, chapter_ID, account_ID) VALUES ('$reaction','$chapter_id','$account_ID')";
         $conn->query($sql);
         echo '1';
     }
     catch(Exception $e){
-        $sql = "DELETE FROM chapters_accounts_reactions WHERE reaction_code='$reaction_code' AND chapter_ID='$chapter_id' AND account_ID='$account_ID'";
+        $sql = "DELETE FROM chapters_accounts_reactions WHERE reaction='$reaction' AND chapter_ID='$chapter_id' AND account_ID='$account_ID'";
         $conn->query($sql);
         echo '-1';
     }
