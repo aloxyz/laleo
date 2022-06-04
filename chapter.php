010195d833190e3bf6033003faa8014b3b88a2c0
Cestnut <hunterkudo@gmail.com>
2022-06-04 22:49:26 +0200

    thoughts shown as trees. Added Reactions. Hide and Delete buttons

--- chapter.php
+++ chapter.php
@@ -1,5 +1,6 @@
 <?php
     require_once("conn.php");
+    require_once("hidden/functions.php");
     session_start();
 
         $chapter_ID = $conn->real_escape_string($_GET['id']);
@@ -30,39 +31,94 @@
             }
 
             #get all existing reactions
-            $sql = "SELECT reaction, reaction_code FROM reactions";
+            $sql = "SELECT reaction FROM reactions";
             if($result = $conn->query($sql))
                 while($tmp = $result->fetch_array(MYSQLI_ASSOC))
                         $reactions[] = $tmp;
 
             #get all thoughts of this chapter and builds thoughts tree
-            $sql = "SELECT thought_ID, thought_padre_ID FROM thoughts WHERE chapter_ID='$chapter_ID'";
+            $sql = "SELECT * FROM thoughts WHERE chapter_ID='$chapter_ID'";
             if($result = $conn->query($sql)){
                 $thoughts = array(); 
                 while($tmp = $result->fetch_array(MYSQLI_ASSOC)){
-                    #print_r($tmp);
                         $tmp['children']=array();
                         $thoughts[$tmp['thought_ID']] = $tmp;
                     }
-                    
-                    print_r($thoughts);
+                
+                #puts children in place
                 foreach ($thoughts as $k => &$v) {
                         if (isset($v['thought_padre_ID'])) {
                           $thoughts[$v['thought_padre_ID']]['children'][] = &$v;
                         }
                       }
-                     #print_r($thoughts);
-                      #print_r($thoughts);
-                foreach ($thoughts as $k => $v) {
-                        #echo $k;
+
+                #deletes all children from root
+                foreach ($thoughts as $k => &$v) {
                     if (isset($v['thought_padre_ID'])){
                         unset($thoughts[$k]);
-                        #print_r($thoughts);
+                        }               
+                      }
+                    }
 
-                        }
-                      }    
-                      #print_r($thoughts);               
-                }
+                #prints comments structure
+                function display_comments(array $comments, $level = 0) {
+                    global $conn;        
+                    global $chapter_ID;
+                    $sql = "SELECT stories.language AS language FROM stories JOIN chapters ON stories.story_ID = chapters.story_ID WHERE chapter_ID='$chapter_ID'";
+                    $row = $conn->query($sql)->fetch_array(MYSQLI_ASSOC);
+                    
+                    foreach ($comments as $thought) {
+                    echo '<div class="row">';
+                        for($i=0; $i<$level*15;$i++) echo '&nbsp;';
+                    echo    '<div class="thought box">
+                                <div class="post-head">
+                                    <p class="post-author">'.$thought['author'].'</p>
+                                </div>
+                                <div class="post-content">';
+                                if(!($thought['hidden_flag']) || (zone_moderator($row['language']) || $_SESSION['role'] == 'admin' || $_SESSION['nickname'] == $thought['author']))
+                                    echo $thought['content'];
+                                else 
+                                    echo 'HIDDEN';
+                    echo    '</div>
+                                <div class="reactions">';
+                                if (zone_moderator($row['language']) || $_SESSION['role'] == 'admin' || $_SESSION['nickname'] == $thought['author'])
+                                    echo "<button>Delete</button>";
+                            
+                            if(zone_moderator($row['language']) || $_SESSION['role']=="admin"){
+                                if($thought['hidden_flag'] == false){
+                                echo '<button class="hide_thought" id="'.$thought['thought_ID'].'">Hide</button>';
+                                echo '<button class="show_thought" id="'.$thought['thought_ID'].'" hidden>Show</button>';
+                                }
+                                else{
+                                  echo '<button class="hide_thought" id="'.$thought['thought_ID'].'" hidden>Hide</button>';
+                                  echo '<button class="show_thought" id="'.$thought['thought_ID'].'">Show</button>';
+                                }
+                              }
+
+                                global $reactions;
+                                foreach($reactions as $reaction){
+                                    $reaction = $reaction['reaction'];
+                                    $sql1 = "SELECT * FROM thoughts_accounts_reactions WHERE thought_ID='$thought[thought_ID]' AND reaction='$reaction'";
+                                    $sql2 = "SELECT * FROM thoughts_accounts_reactions WHERE thought_ID='$thought[thought_ID]' AND reaction='$reaction' AND account_ID='$_SESSION[id]'";
+                                    if($conn->query($sql2)->num_rows)
+                                        echo '<div class="reactions react react_thought btn-primary" thought_ID="'.$thought['thought_ID'].'" reaction="'.$reaction.'">
+                                                <div id="thought_reactions_number'.$reaction.$thought['thought_ID'].'">'.$conn->query($sql1)->num_rows.'</div>
+                                                '.$reaction.'
+                                                </div>';
+                                    else
+                                        echo '<div class="reactions react react_thought" thought_ID="'.$thought['thought_ID'].'" reaction="'.$reaction.'">
+                                                <div id="thought_reactions_number'.$reaction.$thought['thought_ID'].'">'.$conn->query($sql1)->num_rows.'</div>
+                                                '.$reaction.'
+                                                </div>';
+                                }
+                            echo      '</div>
+                                    </div>
+                                </div>';
+                      if (!empty($thought['children'])) {
+                        display_comments($thought['children'], $level + 1);
+                      }
+                    }
+                  }
 
 ?>
 
@@ -102,6 +158,50 @@ $(document).ready(function(){
     )
     });
 
+    $(".react_thought").on("click", function(){
+
+        var reaction = $(this).attr('reaction');
+        var thought_ID = $(this).attr('thought_ID');
+        $(this).toggleClass("btn-primary");
+
+        $.post("hidden/react_thought.php",
+        {
+            thought_id:thought_ID,
+            reaction:reaction
+        },
+        function(data,status){
+            document.getElementById("thought_reactions_number"+reaction+thought_ID).innerHTML = 
+            parseInt(document.getElementById("thought_reactions_number"+reaction+thought_ID).innerHTML)+parseInt(data);  
+        }
+        )
+    });
+
+    $(".show_thought").on("click", function(){
+
+        var thought_ID = $(this).attr('id');
+        $("#"+thought_ID+".hide_thought").attr('hidden', false);
+        $("#"+thought_ID+".show_thought").attr('hidden', true);
+
+        $.post("hidden/toggle_hide_thought.php",
+        {
+        thought_id:thought_ID,
+        bool:0
+        }
+    )});
+
+$(".hide_thought").on("click", function(){
+
+        var thought_ID = $(this).attr('id');
+        $("#"+thought_ID+".hide_thought").attr('hidden', true);
+        $("#"+thought_ID+".show_thought").attr('hidden', false);
+
+        $.post("hidden/toggle_hide_thought.php",
+        {
+        thought_id:thought_ID,
+        bool:1
+        }
+    )});
+
 });
 
 </script>
@@ -124,18 +224,17 @@ $(document).ready(function(){
             <div class="reactions">
             <?php 
                 foreach($reactions as $reaction){
-                $reaction_code = $reaction['reaction_code'];
                 $reaction = $reaction['reaction'];
-                $sql1 = "SELECT * FROM chapters_accounts_reactions WHERE chapter_ID='$chapter_ID' AND reaction_code='$reaction_code'";
-                $sql2 = "SELECT * FROM chapters_accounts_reactions WHERE chapter_ID='$chapter_ID' AND reaction_code='$reaction_code' AND account_ID='$_SESSION[id]'";
+                $sql1 = "SELECT * FROM chapters_accounts_reactions WHERE chapter_ID='$chapter_ID' AND reaction='$reaction'";
+                $sql2 = "SELECT * FROM chapters_accounts_reactions WHERE chapter_ID='$chapter_ID' AND reaction='$reaction' AND account_ID='$_SESSION[id]'";
                 if($conn->query($sql2)->num_rows)
-                    echo '<div class="reactions react react_chapter btn-primary" reaction="'.$reaction_code.'">
-                                <div id="chapter_reactions_number'.$reaction_code.'">'.$conn->query($sql1)->num_rows.'</div>
+                    echo '<div class="reactions react react_chapter btn-primary" reaction="'.$reaction.'">
+                                <div id="chapter_reactions_number'.$reaction.'">'.$conn->query($sql1)->num_rows.'</div>
                             '.$reaction.'
                             </div>';
                 else
-                    echo '<div class="reactions react react_chapter" reaction="'.$reaction_code.'">
-                                <div id="chapter_reactions_number'.$reaction_code.'">'.$conn->query($sql1)->num_rows.'</div>
+                    echo '<div class="reactions react react_chapter" reaction="'.$reaction.'">
+                                <div id="chapter_reactions_number'.$reaction.'">'.$conn->query($sql1)->num_rows.'</div>
                             '.$reaction.'
                             </div>';
                 }
@@ -146,32 +245,7 @@ $(document).ready(function(){
         <div id="total_votes"><?php echo $row['total_votes'];?></div>      
         <div chap_id=<?php echo $_GET['id'];?>><button class="vote" id="down">DOWN</button></div>
         <div class="thoughts">
-            <p class="title">Thoughts</p>
-            <div class="box">
-                <div class="thought">
-                    <div class="post-head">
-                        <p class="post-author">Pippo Baudo</p>
-                    </div>
-                    <div class="post-content">
-                        Very nice story, I enjoyed it a lot :)
-                    </div>
-                    <div class="reactions">
-                        <p class="react">2 ❤️</p>
-                    </div>
-                </div>
-            </div>
-            <div class="box">
-                <div class="thought">
-                    <div class="post-head">
-                        <p class="post-author">Claudio Bisio</p>
-                    </div>
-                <div class="post-content">
-                    This is some lame ass shit bro
-                </div>
-                <div class="reactions">
-                    <p class="react">6 💩</p>
-                </div>
-            </div>
+            <?php display_comments($thoughts); ?>
         </div>
     </div>
 </body>
